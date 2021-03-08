c612774117fd3ccd2751c9a8c2e6bfe930774c18
cutajarj <james.cutajar@gmail.com>
2021-03-08 08:58:26 +0000

    Tape Equilibrium

---
+++ src/timecomplexity/TapeEquilibrium.js
@@ -0,0 +1,22 @@
+/**
+ * This is the solution for Time Complexity > Tape Equilibrium
+ * This is marked as PAINLESS difficulty
+ */
+
+
+function solution(A) {
+    leftSum = A[0];
+    rightSum = 0;
+    A.slice(1).forEach(number => rightSum += number);
+
+    diff = Math.abs(leftSum - rightSum);
+    for (i = 1; i < A.length - 1; i++) {
+        leftSum += A[i];
+        rightSum -= A[i];
+        currentDiff = Math.abs(leftSum - rightSum);
+        if (diff > currentDiff) diff = currentDiff;
+    }
+    return diff;
+}
+
+console.log(solution([3, 1, 2, 4, 3]));
