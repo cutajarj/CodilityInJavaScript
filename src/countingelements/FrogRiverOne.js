c7beb3d4e59f9157af3e05564968c9002434c751
cutajarj <james.cutajar@gmail.com>
2021-03-08 08:39:47 +0000

    Description and formatting

--- src/countingelements/FrogRiverOne.js
+++ src/countingelements/FrogRiverOne.js
@@ -1,3 +1,8 @@
+/**
+ * This is the solution for CountingElements > FrogRiverOne
+ * This is marked as PAINLESS difficulty
+ */
+
 
 function solution(X, A) {
     var river_positions = new Array(X + 1).fill(false);
@@ -6,11 +11,12 @@ function solution(X, A) {
         if (!river_positions[pos]) {
             river_positions[pos] = true;
             X -= 1;
-            if (X === 0) return time
+            if (X === 0) return time;
         }
     }
-    return -1
+    return -1;
 }
+
 
 console.log(solution(5, [1, 3, 1, 4, 2, 3, 5, 4]))
 
