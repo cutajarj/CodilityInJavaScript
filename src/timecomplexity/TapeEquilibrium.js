43f4d3fe8d50dbbb2ddcf7061519a458bd64c381
cutajarj <james.cutajar@gmail.com>
2021-03-14 17:22:49 +0000

    spacing TapeEquilibrium

--- src/timecomplexity/TapeEquilibrium.js
+++ src/timecomplexity/TapeEquilibrium.js
@@ -7,7 +7,7 @@
 function solution(A) {
     let leftSum = A[0];
     let rightSum = 0;
-    A.slice(1).forEach(number => rightSum += number)
+    A.slice(1).forEach(number => rightSum += number);
 
     let diff = Math.abs(leftSum - rightSum);
     for (let i = 1; i < A.length - 1; i++) {
@@ -18,5 +18,6 @@ function solution(A) {
     }
     return diff;
 }
+
 
 console.log(solution([3, 1, 2, 4, 3]));
